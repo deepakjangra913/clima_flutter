6dddc11af5595c4d6cf584349de16d276f9d29e5
Deepak <Deepakjangra605@gmail.com>
2026-06-25 22:18:03 +0530

    added kdoc for some files

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -2,7 +2,21 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+
+/// A helper class responsible for making HTTP GET requests
+/// and decoding the JSON response.
+///
+/// This class is used to simplify network operations by
+/// encapsulating the request and JSON parsing logic.
+///
+/// Example:
+/// ```dart
+/// final networkHelper = NetworkHelper(url);
+/// final data = await networkHelper.getData();
+/// ```
 class NetworkHelper {
+
+  /// The endpoint URL to which the GET request will be sent.
   final String url;
 
   NetworkHelper(this.url);
